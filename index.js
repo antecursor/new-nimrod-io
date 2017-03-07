1a9e65c0e6cf2ffca650881025cbd1a9441d0ec0
Luke Laws <luke@antecursor.com>
2017-03-07 02:33:04 -0500

    sufficient

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 var root = document.body
-m.render(root, "My first app")
+
+var home = m('div', [
+  m('div.cover', [
+    m('img.cover-logo', {src: './img/nimrod_logo.svg'})
+  ]),
+  m('div.contact', [
+    m('div.contact-text', [
+      m('p.h3.center.justify', 'Built from the ground up for the real time web - nimrod delivers a modern, collaborative workspace to help you manage your facilities.  It is currently managing thousands of devices in production as part of an ongoing closed beta.  Nimrod is made available under the highly permissive Mozilla Public License.  To find out if Nimrod can be used in your facility:'),
+      m('a', {href: 'mailto:sales@nimrod.io'}, [
+        m('button.btn.right', 'Contact Us')
+      ])
+    ])
+  ])
+]);
+
+m.render(root, home);
