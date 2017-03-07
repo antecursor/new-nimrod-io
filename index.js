f6746d1d6e90f849ccbec823de67105330cf2b29
Luke Laws <luke@antecursor.com>
2017-03-07 08:51:15 -0500

    git ignore

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var home = m('div', [
   ]),
   m('div.contact', [
     m('div.contact-text', [
-      m('p.h3.center.justify', 'Built from the ground up for the real time web - nimrod delivers a modern, collaborative workspace to help you manage your facilities.  It is currently managing thousands of devices in production as part of an ongoing closed beta.  Nimrod is made available under the highly permissive Mozilla Public License.  To find out if Nimrod can be used in your facility:'),
+      m('p.h3.center.justify', 'Built from the ground up for the real time web - Nimrod delivers a modern, collaborative workspace to help you manage your facilities.  It is currently managing thousands of devices in production as part of an ongoing closed beta.  Nimrod is made available under the highly permissive Mozilla Public License.  To find out if Nimrod can be used in your facility:'),
       m('a', {href: 'mailto:sales@nimrod.io'}, [
         m('button.btn.right', 'Contact Us')
       ])
